6fcd01f69d4314ae3e941d28096e29cb6e91b7f1
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-09 22:59:39 +0900

    useStateを用いてLoadingの状態を管理し、画面に表示できるよう実装

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      { loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <h1>Success to get PokeDatas</h1>
+      )}
+    </div>
   );
 }
 
