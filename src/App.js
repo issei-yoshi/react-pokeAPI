6f07e9f35e56427aa039f55d5d93de28143ec5e7
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-09 23:33:22 +0900

    Cardコンポーネントを作成し、取得したpokemonの情報を元に詳細を表示

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 import { getAllPokemon, getPokemon } from './utils/pokemon';
+import Card from './components/Card';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
