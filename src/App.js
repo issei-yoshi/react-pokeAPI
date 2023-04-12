3a570c5b3ebea5bf5df0f0b7f75847e5bec831d4
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-12 12:05:35 +0900

    Navbarを作成してコンポーネントをインポート

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import { getAllPokemon, getPokemon } from './utils/pokemon';
 import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
@@ -29,19 +30,22 @@ function App() {
   };
 
   return (
-    <div className="App">
-      { loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <>
-          <div className='pokemonCardContainer'>
-            {pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />;
-            })}
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        { loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            <div className='pokemonCardContainer'>
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
